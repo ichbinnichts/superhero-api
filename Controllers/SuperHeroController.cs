2f66b2328e88179dd1a6fdc7f305de0a3398134a
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-01-30 13:40:59 -0300

    change the place of the List heroes

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -6,15 +6,16 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class SuperHeroController : Controller
     {
+        private static List<SuperHero> heroes = new List<SuperHero>
+        {
+            new SuperHero { Id = 1, Name = "Spider man",
+                    FirstName= "Peter", LastName="Parker",
+                    Place="New York City" }
+        };
+        
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> Get()
         {
-            var heroes = new List<SuperHero>
-            {
-                new SuperHero { Id = 1, Name = "Spider man",
-                    FirstName= "Peter", LastName="Parker",
-                    Place="New York City" }
-            };
             return Ok(heroes);
         }
     }
