50bd5f9466ee9ac251670abb0bbb965c092d4fc7
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-01-30 13:31:27 -0300

    add SuperHeroController

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero { Id = 1, Name = "Spider man",
+                    FirstName= "Peter", LastName="Parker",
+                    Place="New York City" }
+            };
+            return Ok(heroes);
+        }
+    }
+}
