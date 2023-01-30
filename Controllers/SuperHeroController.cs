e2c5bde73eddc581fc21bc262fb1ce9b0a12cd7c
Nathan Víctor de Faria <nathan.faria@inatel.br>
2023-01-30 13:50:44 -0300

    add another hero to the heroes list

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -8,14 +8,31 @@ namespace SuperHeroAPI.Controllers
     {
         private static List<SuperHero> heroes = new List<SuperHero>
         {
-            new SuperHero { Id = 1, Name = "Spider man",
-                    FirstName= "Peter", LastName="Parker",
-                    Place="New York City" }
+            new SuperHero { 
+                    Id = 1,
+                    Name = "Spider man",
+                    FirstName= "Peter",
+                    LastName="Parker",
+                    Place="New York City" },
+
+            new SuperHero { 
+                    Id = 2,
+                    Name="Iron man",
+                    FirstName="Tony",
+                    LastName="Stark",
+                    Place="Long Island"}
         };
         
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> Get()
         {
+            return Ok(heroes);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> addHero(SuperHero hero)
+        {
+            heroes.Add(hero);
             return Ok(heroes);
         }
     }
